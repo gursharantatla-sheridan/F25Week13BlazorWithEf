36fff7497876316676ecdc92018b222c0c742cd0
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-12-03 09:27:23 -0500

    conn str

---
+++ F25Week13BlazorWithEf/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using F25Week13BlazorWithEf.Models;
+
+namespace F25Week13BlazorWithEf.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // constructor
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+
+        // entity sets
+        public DbSet<Product> Products { get; set; }
+
+        // data seeding
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Laptop", Price = 1000 },
+                new Product { Id = 2, Name = "Mobile", Price = 1500 },
+                new Product { Id = 3, Name = "Tablet", Price = 600 }
+            );
+        }
+    }
+}
---
+++ F25Week13BlazorWithEf/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace F25Week13BlazorWithEf.Models
+{
+    public class Product
+    {
+        // scalar
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double? Price { get; set; }
+    }
+}
